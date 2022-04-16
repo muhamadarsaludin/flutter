edbbb4b5b8e88f7812269fe256b63b1279478237
muhamad.arsaludin <muhamad.arsaludin@gdn-commerce.com>
2022-04-17 05:27:24 +0700

    Day 01 | Installation dart and flutter

---
+++ test_installasi_dart/bin/test_installasi_dart.dart
@@ -0,0 +1,5 @@
+import 'package:test_installasi_dart/test_installasi_dart.dart' as test_installasi_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${test_installasi_dart.calculate()}!');
+}
---
+++ test_installasi_dart/test/test_installasi_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:test_installasi_dart/test_installasi_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
