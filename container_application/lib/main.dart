f94230dc527c06f927b85ed51547b47c2399a441
muhamad.arsaludin <muhamad.arsaludin@gdn-commerce.com>
2022-04-17 23:46:28 +0700

    Day 02 | widget text, row, coloumn, and container

---
+++ container_application/lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Latihan Container"),
+        ),
+        body: Container(
+          color: Colors.red,
+          margin: EdgeInsets.fromLTRB(25, 10, 15, 20),
+          padding: EdgeInsets.all(10),
+          child: Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: <Color>[Colors.amber, Colors.blue])),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- hello_world_application/lib/main.dart
+++ hello_world_application/lib/main.dart
@@ -10,12 +10,23 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text("Aplikasi Hello World"),
-        ),
-        body: Center(child: Text("Hello World")),
-      ),
-    );
+        home: Scaffold(
+            appBar: AppBar(
+              title: const Text("Aplikasi Hello World"),
+            ),
+            body: Center(
+              child: Container(
+                color: Colors.lightBlue,
+                width: 150,
+                height: 100,
+                child: Text("Saya sedang melatih kemampuan flutter saya",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontStyle: FontStyle.italic,
+                      fontWeight: FontWeight.w700,
+                      fontSize: 20,
+                    )),
+              ),
+            )));
   }
 }
